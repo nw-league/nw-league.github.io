880e4e60035a7efd43796cce3e5fea82beb8b841
Sirref <sirref@ymail.com>
2025-06-29 01:18:49 -0400

    added placeholders and added a 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Navbar from './components/organisms/navbar';
 
 import WarDetail from './pages/war-detail'
 import Wars from './pages/wars';
+import Companies from './pages/companies';
+import Players from './pages/players';
+import NotFound from './pages/notfound';
 
 const App: React.FC = () => {
   return (
@@ -13,15 +16,15 @@ const App: React.FC = () => {
         <Navbar />
         <main className="flex-grow  pt-16">
           <Routes>
-
+            <Route path="/players" element={<Players />} />
+            <Route path="/companies" element={<Companies />} />
             <Route path="/wars" element={<Wars />} />
             <Route path="/wars/:warId" element={<WarDetail />} />
-
-
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
       </div>
-    </HashRouter>
+    </HashRouter >
 
   );
 };
---
+++ src/pages/notfound.tsx
@@ -0,0 +1,15 @@
+// src/pages/NotFound.tsx
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NotFound: React.FC = () => {
+    return (
+        <div className="text-center text-white p-8">
+            <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+            <p className="mb-4">The page you are looking for does not exist.</p>
+            <NavLink to="/wars" className="text-blue-400 underline">Go back home</NavLink>
+        </div>
+    );
+};
+
+export default NotFound;
