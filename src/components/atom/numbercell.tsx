57438b512d5b2324216667fffd27500c938e7021
Sirref <sirref@ymail.com>
2025-07-01 12:21:20 -0400

    add classname option to numbercell

--- src/components/atom/numbercell.tsx
+++ src/components/atom/numbercell.tsx
@@ -4,10 +4,11 @@ import { useMediaQuery } from "react-responsive";
 
 export interface NumberCellPro {
     value: number;
+    className?: string
 }
 
-const NumberCell: React.FC<NumberCellPro> = ({ value }) => {
+const NumberCell: React.FC<NumberCellPro> = ({ value, className }) => {
     const isSmall = useMediaQuery({ maxWidth: 768 })
-    return (<span>{isSmall ? formatCompact(value) : formatThousands(value)}</span>);
+    return (<span className={className}>{isSmall ? formatCompact(value) : formatThousands(value)}</span>);
 };
 export default NumberCell
